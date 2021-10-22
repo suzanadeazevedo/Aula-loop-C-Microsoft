c811b472bfd9d2e0b609c310e91c94077a2581a8
suzanadeazevedo <suzanadeazevedo@hotmail.com>
2021-10-22 19:12:20 -0300

    Add modelos

--- cadeia de caracteres.cs
+++ cadeia de caracteres.cs
@@ -34,3 +34,42 @@ else
     Console.WriteLine("O resultado é menor que 10.");
     Console.WriteLine(" Ou o primeiro numero não é igual ao segundo");
 }
+
+
+int contar = 0;
+do
+{
+    Console.WriteLine($"Olá mundo! a conta é {contar}");
+    contar++;
+}
+while (contar <= 10);
+
+
+
+for (int counter = 0; counter <= 10; counter++)
+{
+    Console.WriteLine($"Ola mundo! A conta é {counter}");
+}
+
+
+//Gerar Linhas//
+for (int row = 1; row < 11; row++)
+{
+    Console.WriteLine($"A linha é {row}");
+}
+
+//Gerar colunas//
+for (char column = '1'; column < '9'; column++)
+{
+    Console.WriteLine($"The column is {column}");
+}
+
+// Formar pares de loop//
+
+for (int row = 1; row < 11; row++)
+{
+    for (char column = 'a'; column < 'k'; column++)
+    {
+        Console.WriteLine($"The cell is ({row}, {column})");
+    }
+}
